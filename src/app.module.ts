99fd9215cd652c90410727ace7ac1a1969ec3629
Kamoliddinbek <kamoliddinxudoyberdiyev72@gmail.com>
2023-08-15 14:10:36 +0500

    role: schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RolesModule } from './roles/roles.module';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { ConfigModule } from '@nestjs/config';
       envFilePath: ".env",
       isGlobal: true
     }),
+    MongooseModule.forRoot(process.env.MONGO_DB),
     RolesModule
   ],
   controllers: [AppController],
---
+++ src/roles/schemas/role.schema.ts
@@ -0,0 +1,22 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+import { type } from 'os';
+
+export type RoleDocument = HydratedDocument<Role>;
+
+@Schema({ versionKey: false })
+export class Role {
+  @Prop({
+    type: String,
+    required: true,
+  })
+  name: string;
+
+  @Prop({
+    type: String,
+    default: '',
+  })
+  description: string;
+}
+
+export const RoleSchema = SchemaFactory.createForClass(Role);
