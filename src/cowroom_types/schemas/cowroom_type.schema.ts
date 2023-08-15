017feff40a62c6afa43912c2142ec27870c65db0
Kamoliddinbek <kamoliddinxudoyberdiyev72@gmail.com>
2023-08-15 15:06:05 +0500

    Cowroom type: schema

---
+++ src/cowroom_types/schemas/cowroom_type.schema.ts
@@ -0,0 +1,21 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type CowroomTypeDocument = HydratedDocument<CowroomType>;
+
+@Schema({ versionKey: false })
+export class CowroomType {
+  @Prop({
+    type: String,
+    required: true,
+  })
+  name: string;
+
+  @Prop({
+    type: String,
+    default: '',
+  })
+  description: string;
+}
+
+export const CowroomTypeSchema = SchemaFactory.createForClass(CowroomType);
