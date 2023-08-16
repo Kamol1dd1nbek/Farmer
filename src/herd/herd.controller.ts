0151942a0ac51abbedbcb8c824fe623feac99f6d
Kamoliddinbek <kamoliddinxudoyberdiyev72@gmail.com>
2023-08-16 13:51:37 +0500

    Herd: service && controller

--- src/herd/herd.controller.ts
+++ src/herd/herd.controller.ts
@@ -1,34 +1,41 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { HerdService } from './herd.service';
 import { CreateHerdDto } from './dto/create-herd.dto';
 import { UpdateHerdDto } from './dto/update-herd.dto';
 
+@ApiTags("Herds")
 @Controller('herd')
 export class HerdController {
-  constructor(private readonly herdService: HerdService) {}
+  constructor(private readonly herdsService: HerdService) {}
 
+  @ApiOperation({ summary: "| Add new herd" })
   @Post()
   create(@Body() createHerdDto: CreateHerdDto) {
-    return this.herdService.create(createHerdDto);
+    return this.herdsService.create(createHerdDto);
   }
 
+  @ApiOperation({ summary: "| Find all herds" })
   @Get()
   findAll() {
-    return this.herdService.findAll();
+    return this.herdsService.findAll();
   }
 
+  @ApiOperation({ summary: "| Find herd" })
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.herdService.findOne(+id);
+    return this.herdsService.findOne(id);
   }
 
+  @ApiOperation({ summary: "| Update herd" })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateHerdDto: UpdateHerdDto) {
-    return this.herdService.update(+id, updateHerdDto);
+    return this.herdsService.update(id, updateHerdDto);
   }
 
+  @ApiOperation({ summary: "| Delete herd" })
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.herdService.remove(+id);
+    return this.herdsService.remove(id);
   }
 }
--- src/herd/herd.service.ts
+++ src/herd/herd.service.ts
@@ -1,26 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, isValidObjectId } from 'mongoose';
+import { Herd } from './schemas/herd.schema';
 import { CreateHerdDto } from './dto/create-herd.dto';
 import { UpdateHerdDto } from './dto/update-herd.dto';
 
 @Injectable()
 export class HerdService {
-  create(createHerdDto: CreateHerdDto) {
-    return 'This action adds a new herd';
+  constructor(
+    @InjectModel(Herd.name) private herdModel: Model<Herd> 
+  ) {}
+
+  async create(createHerdDto: CreateHerdDto): Promise<Herd> {
+    let { name } = createHerdDto;
+    name = name.toUpperCase();
+    const herd = await this.herdModel.create({ ...createHerdDto, name });
+    return herd;
   }
 
-  findAll() {
-    return `This action returns all herd`;
+  async findAll(): Promise<Herd[]> {
+    const herds = await this.herdModel.find({}).populate("supervisor_id");
+
+    if ( herds.length === 0 ) {
+      throw new NotFoundException("Herds not found");
+    }
+    return herds;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} herd`;
+  async findOne(id: string): Promise<Herd> {
+    
+    if ( !isValidObjectId(id) ) {
+      throw new BadRequestException("Invalid Id");
+    }
+
+    const herd = await this.herdModel.findById(id).populate("supervisor_id");
+    if ( !herd ) {
+      throw new NotFoundException(`Herd not found this id: ${id}`);
+    }
+    return herd;
   }
 
-  update(id: number, updateHerdDto: UpdateHerdDto) {
-    return `This action updates a #${id} herd`;
+  async update(id: string, updateHerdDto: UpdateHerdDto): Promise<Herd> {
+
+    if ( !isValidObjectId(id) ) {
+      throw new BadRequestException("Invalid Id");
+    }
+    
+    const updatedHerd = await this.herdModel.findOneAndUpdate({ _id: id }, updateHerdDto, { new: true });
+
+    if ( !updatedHerd ) {
+      throw new NotFoundException("Herd not found");
+    }
+    return updatedHerd;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} herd`;
+  async remove(id: string) {
+
+    if ( !isValidObjectId(id) ) {
+      throw new BadRequestException("Invalid Id");
+    }
+
+    const removedHerd = await this.herdModel.findOneAndDelete({ _id: id });
+
+    if ( !removedHerd ) {
+      throw new NotFoundException("Herd not found");
+    }
+    return removedHerd;
   }
 }
