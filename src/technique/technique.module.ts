05a9d5ed721fc75492661f6fc37ad8da2999ba40
Kamoliddinbek <kamoliddinxudoyberdiyev72@gmail.com>
2023-08-19 15:19:45 +0500

    Technique: module

--- src/technique/technique.module.ts
+++ src/technique/technique.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TechniqueService } from './technique.service';
 import { TechniqueController } from './technique.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Technique, TechniqueSchema } from './schemas/technique.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Technique.name, schema: TechniqueSchema },
+    ]),
+  ],
   controllers: [TechniqueController],
   providers: [TechniqueService],
 })
